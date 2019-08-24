649a87f4285fc143c842e5f4514d1a105055447d
Глеб <glebfen@gmail.com>
2019-08-24 16:30:16 +0200

    Categories on the main page

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php get_header(); ?>
+<?php
+global $post;
+$args = array(
+    'post_type' => 'object',
+    'publish' => true,
+    'paged' => get_query_var('paged'),
+);
+query_posts($args);
+while( have_posts() ) : the_post();
+    $category = get_term_by( 'slug', the_title('','', false), 'product_cat' );
+    echo $category->name;
+endwhile;
+?>
