0fe257d0084dbbf96b2dbfcd21c5a5e7759a3bbe
Глеб <glebfen@gmail.com>
2019-08-31 13:01:22 +0200

    Basket

--- cart/cart.php
+++ cart/cart.php
@@ -70,9 +70,7 @@ do_action( 'woocommerce_before_cart_table' ); ?>
 					<div class="commodities-basket__additional">
 						<div class="commodities-basket__quantity">
 							<div class="commodities-basket__quantity-text">
-								<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $_product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
-									<button style="background: none; border: none;" name="add-to-cart" type="submit" value="<?php echo esc_attr( $_product->get_id() ); ?>"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgMTBoLTEwdi0xMGgtNHYxMGgtMTB2NGgxMHYxMGg0di0xMGgxMHoiLz48L3N2Zz4="></button>
-								</form>
+								<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgMTBoLTEwdi0xMGgtNHYxMGgtMTB2NGgxMHYxMGg0di0xMGgxMHoiLz48L3N2Zz4=">
 								<?php echo $cart_item['quantity']; ?>
 								<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMCAxMGgyNHY0aC0yNHoiLz48L3N2Zz4=">
 							</div>
@@ -97,159 +95,98 @@ do_action( 'woocommerce_before_cart_table' ); ?>
 		 */
 		 do_action( 'woocommerce_cart_collaterals' );
 	?>
-</section>
-<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
-	<?php do_action( 'woocommerce_before_cart_table' ); ?>
+	<form type = 'POST'>
+		<div class="order row">
+			<div class="order__info col-lg-7 col-md-12">
+				<div class="section-index__head section-factories__head">
+					<div class="section-index__title-block">
+						<h2 class="section-index__title">ОФОРМЛЕНИЕ ЗАКАЗА</h2>
+					</div>
+				</div>
+				<div class="section-index__underline section-factories__underline section-basket__underline"></div>
+				<div class="order__title">ФИО</div>
+				<div class="order__input">
+					<input type="text" name="name">
+				</div>
+				<div class="order__title">ТЕЛЕФОН</div>
+				<div class="order__input">
+					<input type="text" name="phone">
+				</div>
+				<div class="order__title">EMAIL</div>
+				<div class="order__input">
+					<input type="email" name="email">
+				</div>
+				<div class="order__title">АДРЕС ДОСТАВКИ</div>
+				<div class="order__input">
+					<input type="text" name="adress">
+				</div>
+				<div class="order__title">КОММЕНТАРИЙ</div>
+				<div class="order__input" style="margin-bottom: 0px">
+					<textarea style="height: 115px" name="comment"></textarea>
+				</div>
+			</div>
+			<div class="order__info col-lg-5 col-md-12 order__right">
+				<div class="section-index__head section-factories__head">
+					<div class="section-index__title-block">
+						<h2 class="section-index__title">ОПЛАТА</h2>
+					</div>
+				</div>
+				<div class="section-index__underline section-factories__underline section-basket__underline"></div>
 
-	<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
-		<thead>
-			<tr>
-				<th class="product-remove">&nbsp;</th>
-				<th class="product-thumbnail">&nbsp;</th>
-				<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-				<th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
-				<th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
-				<th class="product-subtotal"><?php esc_html_e( 'Total', 'woocommerce' ); ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php do_action( 'woocommerce_before_cart_contents' ); ?>
+				<form class="payment_type" action="index.html" method="post">
+					<div class="row">
+						<label class="" for="#ayment_after_delivery">Оплата наличными курьеру</label>
+						<input checked type="radio" name="payment_type" value="cash" id="payment_after_delivery">
+					</div>
+					<br>
+					<div class="row">
 
-			<?php
-			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-				$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
-				$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
-				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-					$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
-					?>
-					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+						<label class="" for="#payment_online">Оплата онлайн</label>
 
-						<td class="product-remove">
-							<?php
-								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-									'woocommerce_cart_item_remove_link',
-									sprintf(
-										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-										esc_html__( 'Remove this item', 'woocommerce' ),
-										esc_attr( $product_id ),
-										esc_attr( $_product->get_sku() )
-									),
-									$cart_item_key
-								);
-							?>
-						</td>
+						<input type="radio" name="payment_type" value="online" id="payment_online">
 
-						<td class="product-thumbnail">
-						<?php
-						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
-
-						if ( ! $product_permalink ) {
-							echo $thumbnail; // PHPCS: XSS ok.
-						} else {
-							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
-						}
-						?>
-						</td>
-
-						<td class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
-						<?php
-						if ( ! $product_permalink ) {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
-						} else {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
-						}
-
-						do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
-
-						// Meta data.
-						echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
-
-						// Backorder notification.
-						if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
-						}
-						?>
-						</td>
-
-						<td class="product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
-							<?php
-								echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-							?>
-						</td>
-
-						<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
-						<?php
-						if ( $_product->is_sold_individually() ) {
-							$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
-						} else {
-							$product_quantity = woocommerce_quantity_input(
-								array(
-									'input_name'   => "cart[{$cart_item_key}][qty]",
-									'input_value'  => $cart_item['quantity'],
-									'max_value'    => $_product->get_max_purchase_quantity(),
-									'min_value'    => '0',
-									'product_name' => $_product->get_name(),
-								),
-								$_product,
-								false
-							);
-						}
-
-						echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
-						?>
-						</td>
-
-						<td class="product-subtotal" data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>">
-							<?php
-								echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-							?>
-						</td>
-					</tr>
-					<?php
-				}
-			}
-			?>
-
-			<?php do_action( 'woocommerce_cart_contents' ); ?>
-
-			<tr>
-				<td colspan="6" class="actions">
-
-					<?php if ( wc_coupons_enabled() ) { ?>
-						<div class="coupon">
-							<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
-							<?php do_action( 'woocommerce_cart_coupon' ); ?>
+					</div>
+					<button type="submit" name="button"  class="btn-add-to-cart" id="continue_basket">Продолжить</button>
+				</form>
+				<div id="make_payment" class="">
+					<div class="order__title">НОМЕР КАРТЫ</div>
+					<div class="order__input">
+						<input type="text" name="card_number">
+						<img src="img/Visa-MasterCard-Maestro-Mir.png" alt="">
+					</div>
+					<div class="order__title">ИМЯ ДЕРЖАТЕЛЯ КАРТЫ</div>
+					<div class="order__input">
+						<input type="text" name="">
+					</div>
+					<div class="row">
+						<div class="col-md-8">
+							<div class="order__title">СРОК ДЕЙСТВИЯ</div>
+							<div class="order__input">
+								<input type="text" name="validity">
+							</div>
 						</div>
-					<?php } ?>
+						<div class="col-md-4">
+							<div class="order__title">CVV 2</div>
+							<div class="order__input">
+								<input type="number" name="cvv">
+							</div>
+						</div>
+						<div class="order__text">
+							В случае если Ваш банк поддерживает технологию безопасного проведения нтернет-платежей Verified By Visa или MasterCard Secure Code, для роведения платежа также может потребоваться ввод специального пароля. Способы и
+							возможность получения паролей для совершения интернет-платежей Вы можете уточнить в банке, выпустившем карту.
+							Настоящий сайт поддерживает 256-битное шифрование. Конфиденциальность сообщаемой персональной информации обеспечивается ПАО "Сбербанк России". Введенная информация не будет предоставлена третьим лицам за исключением
+							случаев,
+							предусмотренных законодательством РФ. Проведение платежей по банковским картам осуществляется в строгом соответствии с
+						</div>
+						<button type="submit" name="button"  class="btn-add-to-cart">Оплатить</button>
+					</div>
+				</div>
+			</div>
+	</form>
+</section>
 
-					<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
-
-					<?php do_action( 'woocommerce_cart_actions' ); ?>
-
-					<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
-				</td>
-			</tr>
-
-			<?php do_action( 'woocommerce_after_cart_contents' ); ?>
-		</tbody>
-	</table>
-	<?php do_action( 'woocommerce_after_cart_table' ); ?>
-</form>
 
 <?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
-
-<div class="cart-collaterals">
-	<?php
-		/**
-		 * Cart collaterals hook.
-		 *
-		 * @hooked woocommerce_cross_sell_display
-		 * @hooked woocommerce_cart_totals - 10
-		 */
-		 do_action( 'woocommerce_cart_collaterals' );
-	?>
-</div>
 
 <?php do_action( 'woocommerce_after_cart' ); ?>
